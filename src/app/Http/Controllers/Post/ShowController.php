c2aeaa8a1d3cfc66fde7c7ac9d516ebd6bd29175
redream <redream.dev@gmail.com>
2025-06-05 22:38:01 +0300

    feature: added predis and caching to /post/{id} GET endpoint

--- src/app/Http/Controllers/Post/ShowController.php
+++ src/app/Http/Controllers/Post/ShowController.php
@@ -5,11 +5,17 @@ namespace App\Http\Controllers\Post;
 use App\Http\Controllers\Controller;
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 class ShowController extends BaseController
 {
-    public function __invoke(Post $post){
-        $post->with(['comments', 'tags'])->with(['users']);
-        return view('post.show', compact('post'));
+    public function __invoke($id){
+        $cacheKey = 'post.' . $id;
+        
+        $postData = Cache::remember($cacheKey, now()->addSeconds(20), function () use ($id) {
+            return Post::with(['comments', 'category', 'tags'])->findOrFail($id);
+        });
+
+        return view('post.show', ['post' => $postData]);
     }
 }
